6906c2ce7a699e21910d0136281dcb3b975056c3
Todd DiFronzo <fonze@aol.com>
2020-09-16 20:18:30 -0700

    updated

--- queue/queue.py
+++ queue/queue.py
@@ -7,8 +7,7 @@ return elements in First In First Out order.
 2. Re-implement the Queue class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Queue tests pass.
-3. What is the difference between using an array vs. a linked list when 
-   implementing a Queue?
+
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
@@ -142,3 +141,6 @@ class Queue:
         self.size -= 1
         value = self.storage.remove_head()
         return value
+
+# 3. What is the difference between using an array vs. a linked list when 
+#    implementing a Queue?
--- stack/stack.py
+++ stack/stack.py
@@ -12,66 +12,112 @@ return elements in Last In First Out order.
 """
 
 # 1. Implement Stack class using an array
-arr = [1,2,3,4,5]
+arr = [1,4,8,16,22]
 
 class Stack:
     def __init__(self):
-        self.storage = []  
-        #self.storage = arr
+        self.size = 0
+        self.storage = []
+        # self.storage = arr 
+
     def __len__(self):
         return len(self.storage)
 
     def push(self, value):
-        self.storage.append(value)
+        return self.storage.append(value)
 
     def pop(self):
         if len(self.storage) == 0:
             return None
-        return self.storage.pop()
+        else:
+            return self.storage.pop()
 
-# # Check
-# arra = Stack()
-# print(arra.storage)
-# arra.push(6)
-# print(arra.storage)
-# arra.pop()
-# print(arra.storage)
-# arra.push(2)
-# arra.push(4)
-# print(arra.storage)
-# print(arra.__len__())
+# test it
+
+print('------------------------')
+arra = Stack()
+print(arra.storage)
+print(arra.__len__())
+arra.push(234)
+arra.push(254)
+arra.push(1)
+arra.push(44)
+print(arra.__str__())
+print(arra.storage)
+arra.pop()
+print(arra.storage)
 
 # 2. Implement a Stack class using Linked List
+# Using class Node and class LinkedList() here instead of importing
 
-# import sys
-# # ('../singly_linked_list')
-# #sys.path.append('C:/Users/Todd/Desktop/data_structures/Data-Structures/singly_linked_list/singly_linked_list.py')            
-# sys.path('/singly_linked_list')
-# from singly_linked_list import LinkedList 
 class Node:
-    def __init__(self, value):
+    def __init__(self, value, next_node=None):
+        # value that the node is holding
         self.value = value
-        self.next = None 
 
-    def __repr__(self):
+        # ref to the next value
+        self.next_node = next_node
+
+    def __str__(self):
+        return str(self.value) 
+
+    def get_value(self):
+        """
+        Method to get a value from the node
+        """
         return self.value 
 
-    # add get method
-    def get_value(self):
-        return self.value
-
-    # get next node
     def get_next(self):
-        return self.next
+        """
+        Method to get a value from the next node
+        """
+        return self.next_node 
 
-    # set next node
     def set_next(self, new_next):
-        self.new_next = new_next
+        """
+        Method to update the node's "next_node" to the new_next
+        """
+        self.next_node = new_next
 
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.tail = None
+        self.tail = None 
+
+    def __str__(self):
+        output = ''
+        current_node = self.head
+        while current_node is not None:
+            output += f'{current_node.value} -->'
+            current_node = current_node.next_node 
+        return output
+
+    def add_to_tail(self, value):
+        """Add to Tail:
+        1. wrap value you are adding in new_node
+        2. check if LL empty
+            a. if LL empty: (check first)
+                    - set self.head to new_node value
+                    - set self.tail to new_node value
+            b. if at least 1 item already in the LL
+                    - update previous last nodes pointer("next_node") to point to the new node (new tail value)
+                    - update self.tail value to the new value we added to the tail
+        """
+        # wrap the value in a new Node
+        new_node = Node(value)
+
+        # check if LL is empty
+        #   a. if empty, set head and tail to the new node
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node 
+
+        #   b. else it already has at least 1 item in it
+        else:
+            # update the last node's "next_node" to the "new_node"
+            self.tail.set_next(new_node)  # last node in chain.next_node = new_node 
+            # update the self.tail value to point to the new_node value we just added
+            self.tail = new_node 
 
     def add_to_head(self, value):
         new_node = Node(value)
@@ -82,68 +128,134 @@ class LinkedList:
             new_node.set_next(self.head)
             self.head = new_node 
 
-    def add_to_tail(self, value):
-        new_node = Node(value)
-        
+    def remove_tail(self):
+        """
+        Remove the last node in the chain and return its value:
+        1. Check if LL empty
+            a. if LL empty: 
+                - set self.head to None
+                - set self.tail to None
+            
+            b. if list has only 1 item:
+                - store the value of the node that we are going to remove
+                - remove the node
+                - set head and tail to None
+                - return the store value
+
+            c. if list has more than 1 item:
+                - store the value of the node that we are going to remove
+                - set the 'self.tail' to the second-to-last node (need to traverse list from start to end)
+                    - starting from head
+                    - keep iterating until the node after "current_node" is the tail
+                    - at end of iteration, set "self.tail" to the "current_node"
+                    - set the new tail's "next_node" to None
+                - return value
+        """
+        # check if empty list to start
         if self.head is None and self.tail is None:
-            self.head = new_node
-            self.tail = new_node 
+            # return None
+            return None
+
+        # check if the list only has one item
+        if self.head == self.tail:
+            # need to store the value of the node we are going to remove
+            value = self.tail.get_value()
+
+            # then remove the node by setting head and tail to None
+            self.head = None
+            self.tail = None 
+
+            # return the stored value
+            return value 
+
+        # otherwise
         else:
-            self.tail.set_next(new_node)
-            self.tail = new_node
+            # store the value of the node we are going to remove
+            value = self.tail.get_value()
+
+        #   We need to set the self.tail to second-to-last-node:
+        #   can only do this by traversing the linked list from beginning to end, can only go forward
+
+            # starting from the head
+            current_node = self.head 
+
+            # keep iterating until the node after "current_node" is the tail - keep track of it until we get to the node before tail node
+            while current_node.get_next() != self.tail:
+                # keep looping
+                current_node = current_node.get_next()
+
+            # at end of iteration, set "self.tail" to the "current_node" 
+            self.tail = current_node
+
+            # set the new tail's "next_node" to None 
+            self.tail.set_next(None)
+
+            # return the value 
+            return value 
 
     def remove_head(self):
-    
+        """Remove head
+        1. check if list is empty
+            a. if empty:
+                return None
+
+            b. if list has only 1 item:
+                - store the value of the node that we are going to remove
+                - remove the node
+                - set head and tail to None
+                - return the store value
+
+            c. if list has more than 1 item
+                - store the old head's value
+                - set self.head to old head's next node
+                - return the value
+        """
+        # check for empty list
         if self.head is None and self.tail is None:
-            return 
-        
-        if not self.head.get_next():
-            head = self.head 
+            # return None
+            return None 
+
+        # check if the list only has one item
+        if self.head == self.tail:
+            # need to store the value of the node we are going to remove
+            value = self.head.get_value()
+
+            # then remove the node by setting head and tail to None
             self.head = None
-            self.tail = None
-            return head.get_value()
+            self.tail = None 
 
-        val = self.head.get_value()
-        self.head = self.head.get_next()
-        return val 
+            # return the stored value
+            return value 
 
-    def llprint(self):
-        printval = self.head
-        while printval is not None:
-            print(printval.value)
-            printval = printval.next
+        else:
+            # store the old head's value  (no need to traverse list this time since starting at head)
+            value = self.head.get_value()
 
-    def remove_tail(self):
+            # set self.head to old head's next
+            self.head = self.head.get_next()
 
-        if self.head is None and self.tail is None:
-            return 
-        current = self.head
-        while current.get_next() is not self.tail:
-            current = current.get_next()
-        val = self.tail.get_value()
-        self.tail = current
-        return val
+            # return the value
+            return value 
 
-# print out a linked list
+
+# Test it
 llist = LinkedList()
 first_node = Node("a")
 second_node = Node("b")
 third_node = Node("c")
 
 llist.head = first_node 
-first_node.next = second_node
-second_node.next = third_node
+first_node.next_node = second_node
+second_node.next_node = third_node 
 
-llist.llprint()
-
-class LLStack:
-    def __init__(self):
+class LLstack:
+    def __init__(self): 
         self.size = 0
-        self.storage = LinkedList() 
+        self.storage = LinkedList()
 
     def __repr__(self):
         return self.storage  
-        
+
     def __len__(self):
         return self.size 
 
@@ -152,15 +264,38 @@ class LLStack:
         self.size += 1
 
     def pop(self):
-        # check to see if empty stack
         if self.size == 0:
             return None
         else:
-            self.size -=1
+            self.size -= 1
             return self.storage.remove_head()
 
-x = LLStack()
-print(x.size)
+    def display(self):
+        iternode = self.storage.head 
+        while iternode != None:
+            print(iternode.value, "->", end="")
+            iternode = iternode.next_node  
 
-x.push(6)
-print(x.storage)
+print('------------------------')   
+x = LLstack()
+# print(x.size)
+# x.push(6)
+# x.push(44)
+# x.push(45)
+# print(x.__str__())
+# print(x.storage)
+
+x.push(8)
+x.push(22)
+x.push("a")
+print(x.display())
+
+"""
+3. What is the difference between using an array vs. a linked list when 
+   implementing a Stack?
+
+Their time complexities will be different.
+    Accessing an element in an array is faster
+    Linked Lists takes linear time so much slower
+
+"""
